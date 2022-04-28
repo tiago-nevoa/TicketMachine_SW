e6539984cc2b3d20889ae9bb6df5ff5ffa683089
TiagoNevoa <tiagonevoa@gmail.com>
2022-04-28 20:37:59 +0100

    Main Test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,19 +2,10 @@ import isel.leic.UsbPort
 
 fun main() {
     HAL.init()
-    while(true){
-        if (HAL.isBit(0b1000_0000)) break
-        
-    }
     var mask = 0b00000001
-    while(mask <= 0b11111111) {
+    while(!HAL.isBit(mask)) {
         // Thread.sleep(1000)
         HAL.setBits(mask)
         mask *=2
-    }
-    while(mask > 0b00000000) {
-        // Thread.sleep(1000)
-        HAL.clrBits(mask)
-        mask /=2
     }
 }
