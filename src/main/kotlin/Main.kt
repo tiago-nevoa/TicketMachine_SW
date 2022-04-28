356b57c392fb34ae8af38eec0815cdd3bcd8fcf4
TiagoNevoa <tiagonevoa@gmail.com>
2022-04-28 20:19:55 +0100

    Clean Rui Test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
 import isel.leic.UsbPort
 
-// Rui teste
 fun main() {
     HAL.init()
+    while(true){
+        if (HAL.isBit(0b1000_0000)) break
+        
+    }
     var mask = 0b00000001
     while(mask <= 0b11111111) {
         // Thread.sleep(1000)
